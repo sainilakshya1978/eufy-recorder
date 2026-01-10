f1416a9d25d54629b21e85b497e60158f9cf959e
sainilakshya1978 <sainilakshya1978@gmail.com>
2026-01-10 15:51:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,8 @@ while True:
     current_hour = now_ist.hour
 
     # Raat 12 AM (0) se Subah 5 AM (5) tak check karega
-    if 0 <= current_hour < 5:
+   # Testing ke liye abhi ka time (15 = 3 PM)
+    if 15 <= current_hour < 16:
         try:
             # 1. Eufy Bridge se check karna ki koi movement hai ya nahi
             # (Note: Agar bridge direct stream de raha hai toh hum FFmpeg chalayenge)
